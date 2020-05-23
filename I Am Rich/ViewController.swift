cc803fed9cf2a28d0aabff8dfbe0196bdaf72104
Alexandre Seidel <arseidel2003@gmail.com>
2020-05-22 23:42:36 -0300

    Meu primeiro app

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Alexandre Seidel on 22/05/20.
+//  Copyright © 2020 Alexandre Seidel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
